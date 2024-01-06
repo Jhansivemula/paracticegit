7453e4b3dd1f44ae50d7778d00cdad235f96a92f
Jhansivemula <155795762+Jhansivemula@users.noreply.github.com>
2024-01-06 10:58:46 +0530

    Add files via upload
    Commit in first

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# paracticegit
-Demo
+# gitdemo2
---
+++ src/test/java/com/githubmaven/Class1.java
@@ -0,0 +1,10 @@
+package com.githubmaven;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+System.out.println("hi i am jhansi i want to become a testing engineer");
+		
+	}
+
+}
